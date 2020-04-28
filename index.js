b381411a13df4e09f1b5e5b5c561beba2a83eb57
leeihou021 <61406271+leeihou021@users.noreply.github.com>
2020-04-28 16:00:11 +0800

    line-robot

---
+++ index.js
@@ -0,0 +1,29 @@
+// 引用linebot
+import linebot from 'linebot'
+// 引用dotnev套件
+import dotenv from 'dotenv'
+
+import rp from 'request-promise'
+
+// 讀取.env檔
+dotenv.config()
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+bot.on('message', async (event) => {
+  let msg = ''
+  try {
+    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
+    msg = data.entry[0].title
+  } catch (error) {
+    console.log(error.message)
+    msg = '發生錯誤'
+  }
+  event.reply(msg)
+})
+bot.listen('/', process.env.PORT, () => {
+  console.log('機器人已啟動')
+})
